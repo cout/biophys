c3f1a9fb8cbbbf3b41addace001470d05ff12098
Paul brannan <curlypaul924@gmail.com>
2011-09-28 23:21:00 -0400

    Simplified (and fixed) intersection check.

--- sim/util.cpp
+++ sim/util.cpp
@@ -39,67 +39,20 @@ Point random_insphere(double rmin, double rmax)
   return p;
 }
 
-// http://paulbourke.net/geometry/sphereline/
+bool is_inside_sphere(Point p, Point sc, double r)
+{
+  p.x -= sc.x;
+  p.y -= sc.y;
+  p.z -= sc.z;
+
+  return p.x*p.x + p.y*p.y + p.z*p.z < r*r;
+}
+
 bool ray_intersects_sphere(Point p1, Point p2, Point sc, double r)
 {
-  double a, b, c;
-  double bb4ac;
-  Point dp;
+  bool p1_inside = is_inside_sphere(p1, sc, r);
+  bool p2_inside = is_inside_sphere(p2, sc, r);
 
-  dp.x = p2.x = p1.x;
-  dp.y = p2.y - p1.y;
-  dp.z = p2.z - p1.z;
-
-  a = (dp.x * dp.x) + (dp.y * dp.y) + (dp.z * dp.z);
-  b = 2 * (dp.x * (p1.x - sc.x) + dp.y * (p1.y - sc.y) + dp.z * (p1.z - sc.z));
-  c = (sc.x * sc.x) + (sc.y * sc.y) + (sc.z * sc.z)
-      + (p1.x * p1.x) + (p1.y * p1.y) + (p1.z * p1.z)
-      - 2 * ((sc.x * p1.x) + (sc.y * p1.y) + (sc.z * p1.z))
-      - (r * r);
-
-  bb4ac = (b * b) - (4 * a * c);
-
-  if (std::fabs(a) < std::numeric_limits<double>::epsilon() || bb4ac < 0)
-  {
-    return false;
-  }
-
-  // TODO: this condition will be reached for every particle in the
-  // system.  Can we somehow avoid doing the sqrt?
-
-  /*
-  double sqrt_bb4ac = std::sqrt(bb4ac);
-  double mu1 = (-b + sqrt_bb4ac) / (2 * a);
-  double mu2 = (-b - sqrt_bb4ac) / (2 * a);
-
-  if ((mu1 >= 0 && mu1 <= 1) || (mu2 >=0 && mu2 <= 1))
-  {
-    if (mu1 == mu2)
-    {
-      // line segment is tangential to the sphere
-      return false;
-    }
-    else
-    {
-      // line segment intersects the sphere
-      return true;
-    }
-  }
-  else
-  {
-    // line segment does not intersect sphere
-    return false;
-  }
-  */
-  double u = ((sc.x-p1.x)*(p2.x-p1.x) + (sc.y-p1.y)*(p2.y-p1.y) + (sc.z-p1.z)*(p2.z-p1.z)) /
-             ((p2.x-p1.x)*(p2.x-p1.x) + (p2.y-p1.y)*(p2.y-p1.y) + (p2.z-p1.z)*(p2.z-p1.z));
-  if (u >= 0 && u <= 1)
-  {
-    return true;
-  }
-  else
-  {
-    return false;
-  }
+  return (p1_inside != p2_inside);
 }
 
